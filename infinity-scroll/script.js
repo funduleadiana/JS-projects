ff13d906a6a1aac98cba71102ce8d0eb8e3e8f47
funduleadiana <diana.fundulea@yahoo.com>
2021-05-05 12:58:07 +0100

    implemented infinite scroll

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,13 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+
 const apiKey = 'u7fTkZxeDg9nGPabVcY4J7LWYLa7HKlnSIFa9qJtzIM'; 
-const count = 50;
+const count = 30;
 let photosArr = [];
 
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+
+function imageLoaded(){
+    imagesLoaded ++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+    }
+
+}
 
 //Helper function to Dry out code
 function setAttributes(element, attributes){
@@ -17,7 +31,8 @@ function setAttributes(element, attributes){
 }
 
 function displayPhotos(){
-
+    imagesLoaded = 0;
+    totalImages = photosArr.length;
     photosArr.forEach((photo)=> {
         const item = document.createElement('a');
         // item.setAttribute('href', photo.links.html);
@@ -36,6 +51,8 @@ function displayPhotos(){
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        img.addEventListener('load', imageLoaded);
+
         item.appendChild(img);
         imageContainer.appendChild(item);
     })
@@ -43,14 +60,23 @@ function displayPhotos(){
 }
 
 async function getPhotos() {
-try{
-    const response = await fetch(apiUrl);
-    photosArr = await response.json()
-    displayPhotos()
-    console.log(photosArr)
-}catch(error){
-    alert('Oops no photos to display', error)
+    try{
+        const response = await fetch(apiUrl);
+        photosArr = await response.json()
+        displayPhotos()
+    
+    }catch(error){
+        alert('Oops no photos to display', error)
+    }
 }
-}
+
+window.addEventListener('scroll', ()=> {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotos();
+    }
+});
+
+
 
 getPhotos();

d313de2f14d1c05a7034d2caad351da28120f812
funduleadiana <diana.fundulea@yahoo.com>
2021-05-05 12:23:43 +0100

    introduced helper function to impreove DRY

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -9,19 +9,33 @@ let photosArr = [];
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
 
+//Helper function to Dry out code
+function setAttributes(element, attributes){
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key])
+    }
+}
 
 function displayPhotos(){
-    
+
     photosArr.forEach((photo)=> {
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
 
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
-
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
         item.appendChild(img);
         imageContainer.appendChild(item);
     })

cb1e616bfe848ea790610f21e466c7ad485ca402
funduleadiana <diana.fundulea@yahoo.com>
2021-05-11 15:19:38 +0100

    generating the bookmarks automatically from JS

--- book-keeper/script.js
+++ book-keeper/script.js
@@ -37,7 +37,39 @@ function validateUrl(nameValue, urlValue){
     return true;
 
 };
+//Building bookmarks
 
+function buildBookmarks(){
+    bookmarks.forEach((bookmark) => {
+        const {name, url} = bookmark;
+        //Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        //close icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas', 'fa-trash-alt');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        //Favicon / Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        //Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt', 'Favicon');
+
+        //Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        //Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+
+    });
+}
 
 function fetchBookmarks(){
     //Get bookmarks if available
@@ -53,6 +85,7 @@ function fetchBookmarks(){
         ];
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
     }
+    buildBookmarks();
 };
 
 

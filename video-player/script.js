10ad2744d542cb1bd356ef3a5e5c8b8ba53ef50c
funduleadiana <diana.fundulea@yahoo.com>
2021-05-12 11:05:40 +0100

    play-pause functionality

--- video-player/script.js
+++ video-player/script.js
@@ -1,7 +1,32 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+function showPlay(){
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
+function playPause(){
+    if(video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    }else{
+        video.pause();
+       showPlay();
+        }
+}
+
+video.addEventListener('ended', showPlay);
 
 
 // Progress Bar ---------------------------------- //
@@ -19,3 +44,8 @@
 // Fullscreen ------------------------------- //
 
 
+
+// Ev listeners 
+
+playBtn.addEventListener('click', playPause);
+video.addEventListener('click', playPause);

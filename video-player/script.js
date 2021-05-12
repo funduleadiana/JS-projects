cf0a1e32b418f0e9535f8087d62d9290242393a7
funduleadiana <diana.fundulea@yahoo.com>
2021-05-12 12:25:00 +0100

    fullscreen and assets

--- video-player/script.js
+++ video-player/script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector('.player');
 const video = document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
@@ -105,9 +106,41 @@ function changeSpeed(){
 
 
 // Fullscreen ------------------------------- //
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen');
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen');
+  }
 
+  let fullscreen = false;
+  
 
-
+  function toggleFullscreen(){
+      if(!fullscreen){
+          openFullscreen(player);
+          
+      }else{
+          closeFullscreen();
+      }
+      fullscreen = !fullscreen;
+  }
 // Ev listeners 
 
 playBtn.addEventListener('click', playPause);
@@ -118,3 +151,4 @@ progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
 speed.addEventListener('click', changeSpeed)
+fullscreenBtn.addEventListener('click', toggleFullscreen);

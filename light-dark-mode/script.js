05ea6183f6bf36ff8dd3641d1719e95c458a3ec5
funduleadiana <diana.fundulea@yahoo.com>
2021-05-16 22:26:44 +0100

    color variation

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -1,10 +1,30 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+
+
+function darkMode(){
+
+    //Instead of writing rgba() we can use rgb (/%);
+    
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    
+
+}
 
 function switchTheme(e){
     if(e.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark')
+        darkMode();
     }else{
         document.documentElement.setAttribute('data-theme', 'light')
+        lightMode();
     }
 }
 

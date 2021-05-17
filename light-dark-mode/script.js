6a0fde1b22bb518bfb37731a52e38b65da9f6d8e
funduleadiana <diana.fundulea@yahoo.com>
2021-05-17 12:31:36 +0100

    refactored code

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -7,6 +7,14 @@ const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
 
+//Dark or Light images 
+function imageMode(color){
+    image1.src= `img/undraw_conceptual_idea_${color}.svg`;
+    image2.src= `img/undraw_feeling_proud_${color}.svg`;
+    image3.src= `img/undraw_proud_coder_${color}.svg`;
+
+}
+
 
 function darkMode(){
 
@@ -16,13 +24,11 @@ function darkMode(){
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     //Manually changing the html elements
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+ 
     
     // Changing the pictures
-    image1.src= 'img/undraw_conceptual_idea_dark.svg';
-    image2.src= 'img/undraw_feeling_proud_dark.svg';
-    image3.src= 'img/undraw_proud_coder_dark.svg';
+    imageMode('dark');
 
 }
 
@@ -33,13 +39,11 @@ function lightMode(){
         textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
         //Manually changing the html elements
         toggleIcon.children[0].textContent = 'Light Mode';
-        toggleIcon.children[1].classList.remove('fa-moon');
-        toggleIcon.children[1].classList.add('fa-sun');
+        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+      
         
         // Changing the pictures
-        image1.src= 'img/undraw_conceptual_idea_light.svg';
-        image2.src= 'img/undraw_feeling_proud_light.svg';
-        image3.src= 'img/undraw_proud_coder_light.svg';
+        imageMode('light')
 }
 
 function switchTheme(e){

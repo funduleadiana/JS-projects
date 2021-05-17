20ca9902883b1f9ea0a94dabac87af2c2f92f1a3
funduleadiana <diana.fundulea@yahoo.com>
2021-05-17 12:50:08 +0100

    refactor code

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -15,46 +15,27 @@ function imageMode(color){
 
 }
 
-
-function darkMode(){
-
-    //Instead of writing rgba() we can use rgb (/%);
-
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+function toggleDarkLightMode(isDark){
+    nav.style.backgroundColor = isDark? 'rgb(0 0 0 / 50%)': 'rgb(255 255 255/ 50%)'
+    textBox.style.backgroundColor = isDark? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     //Manually changing the html elements
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
- 
+    toggleIcon.children[0].textContent = isDark? 'Dark Mode' :'Light Mode';
+    isDark? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  
+    isDark? imageMode('dark') :  imageMode('light');
     
-    // Changing the pictures
-    imageMode('dark');
-
 }
 
-function lightMode(){
-   
-    
-        nav.style.backgroundColor = 'rgb(255 255 255/ 50%)';
-        textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-        //Manually changing the html elements
-        toggleIcon.children[0].textContent = 'Light Mode';
-        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-      
-        
-        // Changing the pictures
-        imageMode('light')
-}
 
 function switchTheme(e){
     if(e.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark')
         localStorage.setItem('theme', 'dark');
-        darkMode();
+        toggleDarkLightMode(true);
     }else{
         document.documentElement.setAttribute('data-theme', 'light')
         localStorage.setItem('theme', 'light');
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 
@@ -70,6 +51,6 @@ if(currentTheme){
 
     if(currentTheme === 'dark'){
         toggleSwitch.checked = true;
-        darkMode();
+        toggleDarkLightMode(true);
     }
-}
+};

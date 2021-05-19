e82b31487a23c36432534345171a60542c46a80a
funduleadiana <diana.fundulea@yahoo.com>
2021-05-19 19:06:13 +0100

    selecting all html elem

--- nasa-api-pictures/script.js
+++ nasa-api-pictures/script.js
@@ -1,4 +1,8 @@
-
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
 
 const count = 15;
 const apiKey = 'DEMO_KEY';
